3fd6f95ae2ff733a827214797355f0dbc783445f
abhaysomani2004 <abhaysomani2004@gmail.com>
2025-01-11 22:53:24 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -18,7 +18,7 @@ mongoose
 
 // Basic Route
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello');
 });
 
 // Start Server
